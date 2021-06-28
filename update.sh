3a541f5ba62e0024e9b88266e4a9895062530829
rachidbch <rachidbch@gmail.com>
2021-06-28 15:43:24 +0200

    Fixed update.sh. Now executes in the repo dir even if executed through a symlink

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+SCRIPT_DIR=$(cd "$(dirname $([ -L ${BASH_SOURCE[0]} ] && readlink -f ${BASH_SOURCE[0]} || echo ${BASH_SOURCE[0]}))" &>/dev/null && pwd -P)
 (
   cd "$SCRIPT_DIR"
   git pull
